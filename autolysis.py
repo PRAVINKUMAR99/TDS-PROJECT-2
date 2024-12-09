47923d6b9814c7b36efd0c3d0e0a5db2b78c9969
PRAVINKUMAR99 <23f2000473@ds.study.iitm.ac.in>
2024-12-09 12:38:30 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1JrGStT2bcVj7IstafP0aRC6HR4a5lndr
 """
 
-!pip install -r requirements.txt
+
 !pip install-r seaborn
 import os
 os.environ["AIPROXY_TOKEN"] = ""
